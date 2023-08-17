411758942fc604e01ef48df7989279c8df95ae31
Yogesh Seenichamy <yogesh.seenichamy.vae2zn@statefarm.com>
2023-08-17 15:21:50 -0400

    Implemented Web Scraping Portion of Project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 
 def get_top_stories():
     res = [] #Stores top 10 stories title, type, source url, and id
@@ -13,8 +14,27 @@ def get_top_stories():
 
     return res
 
-def web_scraper():
-    pass
+def web_scraper(stories):
+    full_texts = {} #Stores title and all text from source url
+
+    for story in stories:
+        #Get Full Text from Story URL
+        res = requests.get(story["url"])
+        soup = bs4.BeautifulSoup(res.text)
+        full_text = soup.body.get_text(" ", strip=True)
+
+        #Create Key-Value Pair between title of Story and Full Text
+        full_texts[story["title"]] = full_text
+
+    return full_texts
+
 
 def text_summarization():
     pass
+
+top_stories = get_top_stories()
+scraped_texts = web_scraper(top_stories)
+
+for k, v in scraped_texts.items():
+    print(k, v)
+    break
